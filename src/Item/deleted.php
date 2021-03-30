26a2ec511020970c056467a06a1ed7589b171b04
@HiruniMalaviarachchi <mail2hiruni@gmail.com>
2021-03-31 00:38:00 +0530

    changes done

--- src/Item/deleted.php
+++ src/Item/deleted.php
@@ -1,27 +1,24 @@
 <?php
 include_once '../../config/connection.php';
-<<<<<<< HEAD
 include_once '../../public/includes/session.php';
 
 
 for($i=0; $i < 6; $i++) {
     
-    $sql =mysqli_query($conn, "INSERT INTO  del_cusorder SELECT * FROM cusorder WHERE id='" . $_GET["id"] . "'"); 
-    $sql = mysqli_query($conn,"DELETE FROM cusorder WHERE id='" . $_GET["id"] . "'");
+    $sql =mysqli_query($conn, "INSERT INTO  del_items SELECT * FROM items WHERE id='" . $_GET["id"] . "'"); 
+    $sql = mysqli_query($conn,"DELETE FROM items WHERE id='" . $_GET["id"] . "'");
 
-    if(!$sql) { die(mysqli_error()); }
-=======
-$sql = "DELETE FROM items WHERE id='" . $_GET["id"] . "'";
-if (mysqli_query($conn, $sql)) {
-    echo '<script type="text/javascript">';
-    echo 'alert("Record deleted successfully");';
-    echo 'window.location.href="ManageItem.php";';
-    echo '</script>';
+    if (mysqli_query($conn, $sql)) {
+        echo '<script type="text/javascript">';
+        echo 'alert("Record deleted successfully");';
+        echo 'window.location.href="ManageItem.php";';
+        echo '</script>';
+    } 
+    $message = base64_encode(urlencode("Record Deleted Successfully"));
+    header('Location:ManageItem.php?msg=' . $message);
+    
+    //if(!$sql) { die(mysqli_error()); }
 
-} else {
-    echo "Error deleting record: " . mysqli_error($conn);
->>>>>>> a8f6915d170ef6d4bbef0fc66fdf0a7b50277bd1
-}
-
+//}
 mysqli_close($conn);
 ?>
