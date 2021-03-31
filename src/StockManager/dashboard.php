1c6ce54776b3aa96f68efc3064ed6e7321fd6507
MohomedSalman <mohamedsalmandeen@gmail.com>
2021-03-31 08:47:48 +0530

    modified

--- src/StockManager/dashboard.php
+++ src/StockManager/dashboard.php
@@ -12,7 +12,7 @@
   <title>Dashboard</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="../Dashboard/dashboard.css" />
+  <link rel="stylesheet" href="../../public/css/dashboard.css" />
   <STYLE>A {text-decoration: none;} </STYLE>
 </head>
 
